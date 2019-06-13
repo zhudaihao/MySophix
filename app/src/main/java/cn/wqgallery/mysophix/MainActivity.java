42281241487b7d3f1cd6d8fdfbe142896f0197d9
zhudaihao <zhudaihao@wswtz.com>
2019-06-13 16:12:39 +0800

    Initial commit

---
+++ app/src/main/java/cn/wqgallery/mysophix/MainActivity.java
@@ -0,0 +1,20 @@
+package cn.wqgallery.mysophix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    /**
+     * 注意添加版本 的版本必须和 APP的build的versionName一致
+     *
+     * 测试补丁是否能用可以使用
+     */
+
+}
